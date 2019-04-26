0b061d6b299253c138c36bc6659a9db4f82ad154
vivek <vsingh135@sapient.com>
2019-04-26 11:34:21 +0100

    second commit- rendering stocks

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,41 @@
+<template>
+    <div> 
+        <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+    </div> 
+</template>
+
+<script>
+import Stock from "./Stock.vue";
+
+export default {
+    data() {
+        return {
+            stocks: [{
+                id: 1,
+                name: "BMW",
+                price: 400
+            }, {
+                id: 2,
+                name: "CISCO",
+                price: 350
+            }, {
+                id: 3,
+                name: "APPLE",
+                price: 500
+            }, {
+                id: 4,
+                name: "NISSAN",
+                price: 600
+            }, {
+                id: 5,
+                name: "RENAULT",
+                price: 650
+            }, ]
+        }
+    },
+    components: {
+        appStock: Stock
+    }
+}
+</script>
+
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Home from "./components/Home.vue";
+import Portfolio from "./components/portfolio/Portfolio.vue"
+import Stocks from "./components/stocks/Stocks.vue"
+
+
+export const routes = [
+    {
+        path: '/', component: Home
+    },
+    {
+        path: '/Portfolio', component: Portfolio
+    },
+    {
+        path: '/Stocks', component: Stocks
+    }
+]
